0c1650e47fe0a7959fc509e2735147317b1e21fe
Hannes Barbez <hannesbarbez@hotmail.com>
2021-08-21 17:34:38 +0200

    Ego;

--- BarbezDotEu.Sudoku.Tests/GeneratorCan.cs
+++ BarbezDotEu.Sudoku.Tests/GeneratorCan.cs
@@ -1,4 +1,7 @@
-﻿using System.Linq;
+﻿// Copyright (c) Hannes Barbez. All rights reserved.
+// Licensed under the GNU General Public License v3.0
+
+using System.Linq;
 using BarbezDotEu.Sudoku.Generator;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
