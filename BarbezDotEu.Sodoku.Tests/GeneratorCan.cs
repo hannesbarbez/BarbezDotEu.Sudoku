3bfa3f27c4a2e6e3c494dd9e026aa2426f9e3e36
Hannes Barbez <hannesbarbez@hotmail.com>
2021-08-21 16:46:10 +0200

    Added unit test;

---
+++ BarbezDotEu.Sodoku.Tests/GeneratorCan.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+using BarbezDotEu.Sodoku.Generator;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BarbezDotEu.Sodoku.Tests
+{
+    [TestClass]
+    public class GeneratorCan
+    {
+        [TestMethod]
+        public void CreateASodoku()
+        {
+            // Arrange + act
+             var sodokuGame = new SodokuGame();
+
+            // Assert something's there
+            Assert.IsNotNull(sodokuGame?.Solution);
+
+            // Assert there are 9 rows in a game
+            Assert.AreEqual(9, sodokuGame.Solution.Count());
+
+            // Asert there are 9 columns in a game
+            Assert.AreEqual(9, sodokuGame.Solution.First().Length);
+
+        }
+    }
+}
